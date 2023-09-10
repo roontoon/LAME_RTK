fc9fdd57676b4525ef3d0e97c7b9a7cc33191f84
roontoon <dkamp@roontoon.com>
2023-09-10 10:23:17 -0400

    Initial Commit

---
+++ LAME5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LAME5
+//
+//  Created by Roontoon on 9/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LAME5/LAME5App.swift
@@ -0,0 +1,17 @@
+//
+//  LAME5App.swift
+//  LAME5
+//
+//  Created by Roontoon on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LAME5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
