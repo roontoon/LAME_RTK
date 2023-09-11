e685226d74eca589d2c3c0ed6d2119d4a89a1441
roontoon <dkamp@roontoon.com>
2023-09-11 16:56:15 -0400

    Initial Commit

---
+++ LAME_RTK/LAME_RTKApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAME_RTKApp.swift
+//  LAME_RTK
+//
+//  Created by Roontoon on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LAME_RTKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
