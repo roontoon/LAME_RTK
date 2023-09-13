9afbd9ea32918e488a624b95ddaf1f6a3b2bd5f7
roontoon <dkamp@roontoon.com>
2023-09-12 21:24:16 -0400

    Bugfix

--- LAME_RTK/GPSDataPoint+CoreDataClass.swift
+++ LAME_RTK/GPSDataPoint+CoreDataClass.swift
@@ -1,15 +1,1 @@
-//
-//  GPSDataPoint+CoreDataClass.swift
-//  Lame_RTK
-//
-//  Created by Roontoon on 9/5/23.
-//
-//
 
-import Foundation
-import CoreData
-
-@objc(GPSDataPoint)
-public class GPSDataPoint: NSManagedObject {
-
-}
